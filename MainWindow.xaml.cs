259bf384c7321f33afc0cc0cc2e7983efb9e13f2
PauloCPSENA <46468376+PauloCPSENA@users.noreply.github.com>
2023-01-17 10:57:43 -0300

    Arquivos soltos

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using ImagemOcorrencia.Models;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImagemOcorrencia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Comment_Ocorrencia> listaComentario { get; set; }
+
+
+        public MainWindow()
+        {
+            listaComentario = new List<Comment_Ocorrencia>();
+            InitializeComponent();
+            this.status.ItemsSource = new List<string> { "Atendida", "Pendente",  };
+            this.disciplina.ItemsSource = new List<string> { "Arquitetura", "Estrutura", "Paisagismo" };
+            this.prioridade.ItemsSource = new List<string> { "Alta", "Normal" };
+            this.faseProjeto.ItemsSource = new List<string> { "Fase 02 - PE - Torre", "Fase 03 - PE - Embasamento", "Fase 04 - LO - Embasamento" };
+            this.responsavel.ItemsSource = new List<string> { "arquitetura@gmail.com", "estrutura@gmail.com", "paisagismo@gmail.com" };
+            FormatDT();
+
+           void FormatDT()
+            {
+                var dt=DateTime.Now;
+                DatApontamento.Text = dt.ToString(("dd/MM/yyyy "));
+            }
+
+
+        }
+
+        private void Main_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void BtnClickComentarios(object sender, RoutedEventArgs e)
+        {
+            for (int contador = 0; contador < 2; contador++)
+            {
+                var exemploComentario = new Comment_Ocorrencia()
+                {
+                    Disciplina = "Arquitetura",
+                    Data = DateTime.Now.ToString("d"),
+                    Comentario = "Teste do comentario",
+                    LinhaBranco = ""
+                };
+                listaComentario.Add(exemploComentario);
+            }
+
+           
+
+            //var exemploComentario1 = new Comment_Ocorrencia()
+            //{
+            //    Disciplina = "Coordenação",
+            //    Data = DateTime.Now.ToString("d"),
+            //    Comentario = "Esse é um outro teste de coordenação dos comentarios maior para ver o que acontece",
+            //    LinhaBranco = ""
+
+            //};
+
+            //listaComentario.Add(exemploComentario1);
+
+            var lista = new List<string> { "Paulo", "Andre", "Tito" };
+
+            Main.Content = new Page1(listaComentario);
+
+        }
+
+        private void BtnOnclickAnexos(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+
+        private void BtnOnclickHistorico(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page3();
+        }
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,94 @@
+﻿using ImagemOcorrencia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImagemOcorrencia
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1(List<Comment_Ocorrencia> Comentario)
+        {
+            // ***********************DEFINE O NUMERO DE LINHAS QUE VAMOS TER COM TODOS COMENTÁRIOS*******
+
+            InitializeComponent();
+            var numeroComentarios = Comentario.Count;
+            var numeroLinhas = Comentario.Count * 3;
+            var b = 0;                 
+
+
+            for (int contador = 0; contador < numeroLinhas; contador++)
+            {
+                b = b + 1;
+
+                RowDefinition rowDef1 = new RowDefinition();
+                ComentarioGrid.RowDefinitions.Add(rowDef1);
+            }
+
+            // ***********************DEFINE OS COMENTÁRIOS DAS OCORRÊNCIAS*******************************
+
+            var row = 0;
+
+            foreach (var item in Comentario)
+            {
+                //----------------------------------------------------Disciplina--------------------------
+                TextBlock disciplina = new TextBlock()
+                {
+                    Text = item.Disciplina
+                };
+                ComentarioGrid.Children.Add(disciplina);
+                Grid.SetColumn(disciplina, 0);
+                Grid.SetRow(disciplina, row * 3);
+                //----------------------------------------------------Data--------------------------------
+                TextBlock data = new TextBlock()
+                {
+                    Text = item.Data
+                };
+                ComentarioGrid.Children.Add(data);
+                data.HorizontalAlignment = HorizontalAlignment.Right;
+                Grid.SetColumn(data, 2);
+                Grid.SetRow(data, row * 3);
+                //----------------------------------------------------Comentário--------------------------
+                TextBlock comentario = new TextBlock()
+                {
+                    Text = item.Comentario
+                };
+
+                ComentarioGrid.Children.Add(comentario);
+                comentario.TextWrapping = TextWrapping.Wrap;
+                Grid.SetColumnSpan(comentario, 3);
+                Grid.SetRow(comentario, row * 3 + 1);
+
+                //----------------------------------------------------Linha em branco--------------------------
+                TextBlock linhaBranco = new TextBlock()
+                {
+                    Text = item.LinhaBranco
+                };
+                ComentarioGrid.Children.Add(linhaBranco);
+                Grid.SetColumnSpan(linhaBranco, 3);
+                Grid.SetRow(linhaBranco, row * 3 + 2);
+
+                row = row + 1;
+
+            }         
+
+        }
+
+    }
+
+}
